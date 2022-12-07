eefe4617e698f97acb6961c3553c25fa6815e831
Nick Chambers <uplime@spookyinternet.com>
2022-12-07 03:57:34 -0600

    Support multiple webhook levels

--- discord-webhook.py
+++ discord-webhook.py
@@ -1,6 +1,7 @@
 import discohook.logger
 import discohook.webhook
 import os
+import sys
 
 def make_pair(msg):
   return [msg, os.getenv(f"INPUT_{msg.upper()}")]
@@ -9,24 +10,35 @@ webhook_url = os.getenv("INPUT_WEBHOOK-URL")
 user = os.getenv("INPUT_USERNAME")
 pfp = os.getenv("INPUT_AVATAR")
 
-msg_types = [
+msg_kinds = [
   "message", "debug", "error",
   "notice", "warn"
 ]
 
-msgs = dict(map(make_pair, msg_types))
+msgs = dict(map(make_pair, msg_kinds))
 used_msgs = list(filter(lambda msg: msg is not None, msgs.values()))
 logger = discohook.logger.Logger()
 
 if len(used_msgs) == 0:
   logger.error("No messages given.")
-  os.exit(1)
+  sys.exit(1)
 
 webhook = discohook.webhook.Webhook(webhook_url, username=user, avatar=pfp)
-res = webhook.wire(msgs["message"])
 
-if res.ok():
-  logger.notice("webhook payload successfully delivered.")
-else:
-  logger.error(f"unexpected code {res.code} received.")
-  os.exit(1)
+for kind, msg in msgs:
+  if msg is None:
+    next
+
+  fn = getattr(webhook, kind)
+
+  if fn is None:
+    logger.warn(f"Could not find suitable message kind for {kind}")
+    next
+
+  res = fn(msg)
+
+  if res.ok():
+    logger.notice("webhook payload successfully delivered.")
+  else:
+    logger.error(f"unexpected code {res.code} received.")
+    sys.exit(1)
