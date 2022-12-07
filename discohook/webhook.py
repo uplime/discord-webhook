86e3168da9827a6c6afd680f508e0dc240a684f9
Nick Chambers <uplime@spookyinternet.com>
2022-12-07 04:33:58 -0600

    Attempt basic embed message

--- discohook/webhook.py
+++ discohook/webhook.py
@@ -13,32 +13,40 @@ class Webhook:
     self.url = url
     self.username = username
     self.avatar = avatar
+
+    self.payload = {
+      "username": self.username,
+      "avatar_url": self.avatar
+    }
+
+#    if self.avatar is not None:
+#      self.payload["avatar_url"] = self.avatar
+
     self.session = requests.Session()
 
     self.session.headers.update({
       "User-Agent": "gh/uplime/discord-webhook"
     })
 
-  def wire(self, msg):
-    data = {
-      "username": self.username,
-      "content": msg
-    }
+  def wire(self, msg, embed=False, color=None, title=None):
+    if embed:
+      self.payload["embeds"] = [
+        {"color": color, "title": title, "description": msg}
+      ]
+    else:
+      self.payload["content"] = msg
 
-    if self.avatar is None:
-      data["avatar_url"] = self.avatar
-
-    res = self.session.post(self.url, data=data)
+    res = self.session.post(self.url, data=self.payload)
     return WebhookResponse(res.status_code, res.content)
 
-  def debug(self, msg):
-    return self.wire(msg)
+  def debug(self, msg, title=None):
+    return self.wire(msg, embed=True, title=title)
 
   def error(self, msg):
-    return self.wire(msg)
+    return self.wire(msg, embed=True, title=title)
 
   def notice(self, msg):
-    return self.wire(msg)
+    return self.wire(msg, embed=True, color=5763719, title=title)
 
   def warning(self, msg):
-    return self.wire(msg)
+    return self.wire(msg, embed=True, title=title)
