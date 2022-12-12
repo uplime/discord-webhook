a881974f21acb00c3f96303f436e668253621ad6
Nick Chambers <uplime@spookyinternet.com>
2022-12-12 03:59:34 -0600

    Fix assignment name

--- discohook/webhook.py
+++ discohook/webhook.py
@@ -19,7 +19,7 @@ class WebhookResponse:
 class Webhook:
   def __init__(self, url, user=None, avatar=None):
     self.url = url
-    self.username = username
+    self.username = user
     self.avatar = avatar
 
     self.payload = {
