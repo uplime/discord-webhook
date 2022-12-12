8a3b2ac09270d095a7210f318b05b1a98ec4e781
Nick Chambers <uplime@spookyinternet.com>
2022-12-12 03:55:49 -0600

    Transparently pass options to print

--- discohook/logger.py
+++ discohook/logger.py
@@ -1,15 +1,15 @@
 class Logger:
-  def emit(self, level, msg):
-    print(f"::{level} ::{msg}")
+  def emit(self, level, msg, **opts):
+    print(f"::{level} ::{msg}", **opts)
 
-  def debug(self, msg):
-    self.emit("debug", msg)
+  def debug(self, msg, **opts):
+    self.emit("debug", msg, **opts)
 
-  def error(self, msg):
-    self.emit("error", msg)
+  def error(self, msg, **opts):
+    self.emit("error", msg, **opts)
 
-  def notice(self, msg):
-    self.emit("notice", msg)
+  def notice(self, msg, **opts):
+    self.emit("notice", msg, **opts)
 
-  def warning(self, msg):
-    self.emit("warning", msg)
+  def warning(self, msg, **opts):
+    self.emit("warning", msg, **opts)
